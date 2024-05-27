c2cc1809127c347d35e098a762e9ef170b4c0024
filippoziliotto <filippo.ziliotto@phd.unipd.it>
2024-05-27 11:47:35 +0200

    adding detr50-101 detectors

--- habitat-baselines/habitat_baselines/rl/ppo/models/models.py
+++ habitat-baselines/habitat_baselines/rl/ppo/models/models.py
@@ -3,10 +3,12 @@ import numpy as np
 from collections import defaultdict
 from transformers import (Owlv2Processor, OwlViTProcessor,
                           Owlv2ForObjectDetection, OwlViTForObjectDetection,
-                          AutoProcessor, AutoModelForZeroShotObjectDetection
+                          AutoProcessor, AutoModelForZeroShotObjectDetection,
+                          DetrImageProcessor, DetrForObjectDetection
                           )
 from habitat_baselines.rl.ppo.utils.utils import save_images_to_disk
 from habitat_baselines.rl.ppo.utils.nms import nms
+from habitat_baselines.rl.ppo.utils.names import class_names_coco, desired_classes_ids
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -17,7 +19,7 @@ class ObjectDetector:
         self.thresh = thresh 
         self.nms_thresh = nms_thresh
 
-        if (type not in ['owl-vit', 'owl-vit2', 'grounding-dino']) or (size not in ['base', 'large']):
+        if (type not in ['owl-vit', 'owl-vit2', 'grounding-dino', 'detr']) or (size not in ['base', 'large', 'resent50','resnet101']):
             raise ValueError("Invalid model settings!")
         
         if type == 'owl-vit2':
@@ -39,7 +41,14 @@ class ObjectDetector:
             self.model_name = f"IDEA-Research/grounding-dino-{size}"
             self.processor = AutoProcessor.from_pretrained(self.model_name)
             self.model = AutoModelForZeroShotObjectDetection.from_pretrained(self.model_name).to(self.device)
-
+        elif type in ["detr"]:
+            assert self.thresh >= 0.99, "DETR model requires a threshold of at least 0.9"
+            if size in ["resnet50"]:
+                self.model_name = "facebook/detr-resnet-50"
+            elif size in ["resnet101"]:
+                self.model_name = "facebook/detr-resnet-101"
+            self.processor = DetrImageProcessor.from_pretrained(self.model_name, revision="no_timm")
+            self.model = DetrForObjectDetection.from_pretrained(self.model_name, revision="no_timm").to(self.device)
 
     def normalize_coord(self,bbox,img_size):
         w,h = img_size
@@ -50,8 +59,13 @@ class ObjectDetector:
         y2 = min(y2,h-1)
         return [x1,y1,x2,y2]
 
-    def predict(self,img, obj_name):
-        if self.type in ["owl-vit", "owl-vit2"]:
+    def pre_process_detection(self, img, obj_name):
+        if self.type in ["detr"]:
+            encoding = self.processor(
+                images=img, 
+                return_tensors='pt')
+            return encoding
+        elif self.type in ["owl-vit", "owl-vit2"]:
             text = [[f'a photo of {obj_name}']]
         elif self.type in ["grounding-dino"]:
             text = f"a photo of {obj_name}"
@@ -60,6 +74,38 @@ class ObjectDetector:
                 text=text, 
                 images=img,
                 return_tensors='pt')
+        return encoding
+
+    def post_process_detection(self, outputs, target_sizes, encoding):
+        if self.type in ["owl-vit", "owl-vit2", "detr"]:
+            results = self.processor.post_process_object_detection(
+                outputs=outputs,
+                threshold=self.thresh,
+                target_sizes=target_sizes)
+        elif self.type in ["grounding-dino"]:
+            results = self.processor.post_process_grounded_object_detection(
+                outputs,
+                encoding['input_ids'],
+                box_threshold=self.thresh,
+                text_threshold=0.3,
+                target_sizes=target_sizes)
+        return results
+
+    def class_ids_to_labels(self, boxes, scores, labels, obj_name):
+        names, bbox, conf = [], [], []
+        if len(boxes)==0:
+            return [], [], []
+        for label in labels:
+            if label in list(desired_classes_ids.keys()):
+                if class_names_coco[desired_classes_ids[label]] not in obj_name:
+                    return [], [], []
+                names.append(class_names_coco[desired_classes_ids[label]])
+                bbox.append(boxes[labels.index(label)])
+                conf.append(scores[labels.index(label)])
+        return bbox, conf, names
+
+    def predict(self,img, obj_name):
+        encoding = self.pre_process_detection(img, obj_name)
         encoding = {k:v.to(self.device) for k,v in encoding.items()}
         with torch.no_grad():
             outputs = self.model(**encoding)
@@ -68,20 +114,15 @@ class ObjectDetector:
                     outputs[k] = v.to('cpu') if isinstance(v, torch.Tensor) else v
         
         target_sizes = torch.Tensor([img.shape[:-1]])
-
-        if self.type in ["owl-vit", "owl-vit2"]:
-            results = self.processor.post_process_object_detection(outputs=outputs,threshold=self.thresh,target_sizes=target_sizes)
-        elif self.type in ["grounding-dino"]:
-            results = self.processor.post_process_grounded_object_detection(
-                outputs,
-                encoding['input_ids'],
-                box_threshold=0.4,
-                text_threshold=0.3,
-                target_sizes=target_sizes)
+        results = self.post_process_detection(outputs, target_sizes, encoding)
 
         boxes, scores = results[0]["boxes"], results[0]["scores"]
         boxes = boxes.cpu().detach().numpy().tolist()
         scores = scores.cpu().detach().numpy().tolist()
+
+        if self.type in ["detr"]:
+            labels = results[0]["labels"].cpu().detach().numpy().tolist()
+            boxes, scores, labels = self.class_ids_to_labels(boxes, scores, labels, obj_name)
 
         if len(boxes)==0:
             detection_dict = {'boxes': [], 'scores': [], 'labels': []}
--- habitat-baselines/habitat_baselines/rl/ppo/utils/names.py
+++ habitat-baselines/habitat_baselines/rl/ppo/utils/names.py
@@ -6,3 +6,21 @@ refined_names = {
     "bed": "bed",
     "chair": "chair",
 }
+
+class_names_coco = {
+    "tv": "tv",
+    "toilet": "toilet seat",
+    "potted_plant": "plant",
+    "couch": "couch",
+    "bed": "bed",
+    "chair": "chair",
+}
+
+desired_classes_ids = {
+    62: "chair",
+    63: "couch",
+    64: "potted_plant",
+    65: "bed",
+    70: "toilet",
+    72: "tv"
+}

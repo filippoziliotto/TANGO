fix value map weighting + frontiers

habitat-baselines/habitat_baselines/rl/ppo/models/models.py
@@ -383,7 +383,7 @@ class ValueMapper:
     _last_frontier: np.ndarray = np.zeros(2)
     _previous_frontier: np.ndarray = np.zeros(2)
 
-    def __init__(self, habitat_env, size):
+    def __init__(self, habitat_env, type, size):
         self.habitat_env = habitat_env
         self._get_cameras_parameters(self.habitat_env.config)
         self.visualize = True
@@ -391,35 +391,39 @@ class ValueMapper:
         self.use_double_value_map = True
 
         self._acyclic_enforcer = AcyclicEnforcer()
+        self._exploration_thresh = 0.0
 
         self.obstacle_map = ObstacleMap(
             agent_radius=self._agent_radius,
-            min_height=0.4,
-            max_height=1.8,
+            min_height=0.61,
+            max_height=self._max_obstacle_height,
             area_thresh=1.5
         )
         self.frontier_map = FrontierMap(
+            type=type,
             size=size, 
             encoding_type="cosine"
         )
         self.value_map = ValueMap(
             value_channels=1,
-            use_max_confidence=True,
+            use_max_confidence=False,
             fusion_type="default",
             obstacle_map=self.obstacle_map
         )
 
     def preprocess_text(self, text):
-        obj = self._prompt.replace("[]", text)
+        # obj = self._prompt.replace("[]", text)
 
-        if self.use_double_value_map:
-            return (obj, self._prompt.split("|")[1])
+        # if self.use_double_value_map:
+        #     return (obj, self._prompt.split("|")[1])
         
-        return self._prompt.replace("[]", text)
+        # return self._prompt.replace("[]", text)
+        return text
         
     def reset_map(self):
         self.frontier_map.reset()
         self.obstacle_map.reset()
+        self.value_map.reset()
 
     def update_map(self, curr_image, text):
 
@@ -442,8 +446,8 @@ class ValueMapper:
         )
 
         self.curr_values = self.frontier_map._encode(
-            curr_image,
-            prompt
+            image = curr_image,
+            text = prompt
         )
 
         self.value_map.update_map(
@@ -452,25 +456,29 @@ class ValueMapper:
             tf_camera_to_episodic = self._get_tf_camera_to_episodic(self.habitat_env),
             min_depth = self._min_depth,
             max_depth = self._max_depth,
-            fov = self._fov
+            fov = np.deg2rad(self._fov),
         )
         self.value_map.update_agent_traj(
-            self.value_map._xy_to_px(self.habitat_env.get_current_observation(type="gps").reshape(1,2))[0],
-            self.habitat_env.get_current_observation(type="compass"),
+            robot_xy = self._get_tf_camera_to_episodic(self.habitat_env)[:2, 3],
+            robot_heading = self.habitat_env.get_current_observation(type="compass"),
         )
 
         self.best_frontier_polar = self._get_best_frontier(
-            self.obstacle_map._get_frontiers()
+            frontiers=self.obstacle_map._get_frontiers()
         )
 
         if self.visualize:
-            map_ = self.obstacle_map.visualize(self._last_frontier)
-            plt.imsave("images/map.png", map_)
+            map_ = self.obstacle_map.visualize(
+                best_frontier = self._last_frontier
+            )
+            cv2.imwrite("images/map.png", map_)
 
             map_ = self.value_map.visualize(
+                # reduce_fn=self._reduce_values,
                 obstacle_map=self.obstacle_map,
+                best_frontier=self._last_frontier
             )
-            plt.imsave("images/value_map.png", map_)
+            cv2.imwrite("images/value_map.png", map_)
 
     def _get_best_frontier(
         self,
@@ -489,6 +497,8 @@ class ValueMapper:
         # The points and values will be sorted in descending order
         sorted_pts, sorted_values = self._sort_frontiers_by_value(frontiers)
         robot_xy = self.habitat_env.get_current_observation(type="gps")
+        heading = self.habitat_env.get_current_observation(type="compass")
+        # robot_xy = self._get_tf_camera_to_episodic(self.habitat_env)[:2, 3]
         best_frontier_idx = None
         top_two_values = tuple(sorted_values[:2])
 
@@ -542,7 +552,7 @@ class ValueMapper:
         self._last_frontier = best_frontier
 
         # We update the target only if the best frontier has changed
-        self.best_frontier_polar = self._get_polar_from_frontier(robot_xy, best_frontier)
+        self.best_frontier_polar = self._get_polar_from_frontier(robot_xy, heading, best_frontier)
 
         return self.best_frontier_polar
 
@@ -556,33 +566,13 @@ class ValueMapper:
         if itm_policy == "v3":
             return self.value_map.sort_waypoints(frontiers, 0.5, reduce_fn=self._reduce_values)
 
-    def _reduce_values(self, values: List[Tuple[float, float]]) -> List[float]:
-        """
-        Reduce the values to a single value per frontier
-
-        Args:
-            values: A list of tuples of the form (target_value, exploration_value). If
-                the highest target_value of all the value tuples is below the threshold,
-                then we return the second element (exploration_value) of each tuple.
-                Otherwise, we return the first element (target_value) of each tuple.
-
-        Returns:
-            A list of values, one per frontier.
-        """
-        target_values = [v[0] for v in values]
-        max_target_value = max(target_values)
-
-        if max_target_value < self._exploration_thresh:
-            explore_values = [v[1] for v in values]
-            return explore_values
-        else:
-            return [v[0] for v in values]
-
-    def _get_polar_from_frontier(self, robot_xy, frontier):
-        # TODO: make this MUCH MORE ELEGANT
-        heading = self.habitat_env.get_current_observation(type="compass")
+    def _get_polar_from_frontier(
+            self,
+            robot_xy: np.ndarray,
+            heading: float,
+            frontier: List[np.ndarray],
+        ):
         frontier_xy = self.value_map._px_to_xy(frontier.reshape(1, 2))[0]
-        # This next line should be correct
         robot_xy = np.array([robot_xy[0], -robot_xy[1]])
         rho, theta = rho_theta(robot_xy, heading, frontier_xy)
         return torch.tensor([[rho, theta]], device="cuda", dtype=torch.float32)
@@ -608,6 +598,7 @@ class ValueMapper:
         self._fx = self._image_width / (2 * np.tan(np.deg2rad(self._fov) / 2))
         self._fy = self._image_height / (2 * np.tan(np.deg2rad(self._fov) / 2))
         self._topdown_view_angle = np.deg2rad(self._fov)
+        self._max_obstacle_height = self._agent_height
      
     def _get_current_depth(self):
         return self.habitat_env.get_current_observation(type='depth')[:,:,0]
@@ -618,19 +609,24 @@ class ValueMapper:
         camera_position = np.array([x, -y, self._camera_height])
         return xyz_yaw_to_tf_matrix(camera_position, camera_yaw)
 
-        # TODO: maybe improve the code elegance of this
-        if hasattr(self, '_previous_best_frontier'):
-            if np.array_equal(self._previous_best_frontier, best_frontier):
-                # If the best frontier is the same as the previous one, do not update
-                self.best_frontier_polar = None
-                return True
-            else:
-                # Update the previous best frontier
-                self._previous_best_frontier = best_frontier
-                self._previous_best_value = best_value
-                return False
+    def _reduce_values(self, values: List[Tuple[float, float]]) -> List[float]:
+        """
+        Reduce the values to a single value per frontier
+
+        Args:
+            values: A list of tuples of the form (target_value, exploration_value). If
+                the highest target_value of all the value tuples is below the threshold,
+                then we return the second element (exploration_value) of each tuple.
+                Otherwise, we return the first element (target_value) of each tuple.
+
+        Returns:
+            A list of values, one per frontier.
+        """
+        target_values = [v[0] for v in values]
+        max_target_value = max(target_values)
+
+        if max_target_value < self._exploration_thresh:
+            explore_values = [v[1] for v in values]
+            return explore_values
         else:
-            # Initialize the previous best frontier
-            self._previous_best_frontier = best_frontier
-            self._previous_best_value = best_value
-            return False
+            return [v[0] for v in values]

habitat-baselines/habitat_baselines/rl/ppo/utils/map/frontier_map.py
@@ -20,9 +20,10 @@ class Frontier:
 class FrontierMap:
     frontiers: List[Frontier] = []
 
-    def __init__(self, size, encoding_type: str = "cosine"):
+    def __init__(self, type, size, encoding_type: str = "cosine"):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.encoder, self.processor = get_value_mapper(self.device, size)
+        self.encoder, self.processor = get_value_mapper(self.device, type, size)
+        self.type = type
 
     def reset(self) -> None:
         self.frontiers = []
@@ -67,16 +68,33 @@ class FrontierMap:
         Returns:
 
         """
-        inputs = self.processor(text=[text], images=image, return_tensors="pt", padding=True).to(self.device)
-
-        # Get the image and text embeddings
-        outputs = self.encoder(**inputs)
-        image_embeddings = outputs.image_embeds
-        text_embeddings = outputs.text_embeds
+        # Process Input
+        if self.type in ['clip']:
+            inputs = self.processor(text=[text], images=image, return_tensors="pt", padding=True).to(self.device)
+        elif self.type in ['blip']:
+            inputs = self.processor(image, text, return_tensors="pt").to(self.device)
 
         # Compute cosine similarity
-        cosine_sim = cosine_similarity(image_embeddings.detach().cpu().numpy(), text_embeddings.detach().cpu().numpy())
-        return cosine_sim[0][0]
+        if self.type in ['clip']:
+            # Get the image and text embeddings
+            outputs = self.encoder(**inputs)
+            image_embeddings = outputs.pixel_values
+            text_embeddings = outputs.last_hidden_state
+            # Get the image and text embeddings
+            outputs = self.encoder(**inputs)
+            image_embeddings = outputs.image_embeds
+            text_embeddings = outputs.text_embeds
+
+            # Compute cosine similarity
+            cosine_sim = cosine_similarity(image_embeddings.detach().cpu().numpy(), text_embeddings.detach().cpu().numpy())[0][0]
+            return cosine_sim
+        
+        elif self.type in ['blip']:
+            cosine_sim = self.encoder(**inputs, use_itm_head=False)[0].detach().cpu().numpy().item()
+            
+        return cosine_sim
+
+        
 
     def sort_waypoints(self) -> Tuple[np.ndarray, List[float]]:
         """

habitat-baselines/habitat_baselines/rl/ppo/utils/map/obstacle_map.py
@@ -171,16 +171,20 @@ class ObstacleMap(BaseMap):
         )
         return frontiers
 
-    def visualize(self, best_frontier=None) -> np.ndarray:
+    def visualize(self, 
+                  best_frontier=None,
+        ) -> np.ndarray:
+        
         """Visualizes the map."""
         vis_img = np.ones((*self._map.shape[:2], 3), dtype=np.uint8) * 255
         # Draw explored area in light green
         vis_img[self.explored_area == 1] = (200, 255, 200)
+
         # Draw unnavigable areas in gray
         vis_img[self._navigable_map == 0] = self.radius_padding_color
         # Draw obstacles in black
         vis_img[self._map == 1] = (0, 0, 0)
-        # Draw frontiers in blue (200, 0, 0)
+        # Draw frontiers in red (200, 0, 0)
         for frontier in self._frontiers_px:
             if best_frontier is not None and np.array_equal(frontier, best_frontier):
                 # Best frontier is drawn in blue

habitat-baselines/habitat_baselines/rl/ppo/utils/map/traj_visualizer.py
@@ -15,7 +15,7 @@ class TrajectoryVisualizer:
     agent_line_thickness: int = 3
     path_color: tuple = (0, 255, 0)
     path_thickness: int = 3
-    scale_factor: float = 1.0
+    scale_factor: float = 0.9
 
     def __init__(self, origin_in_img: np.ndarray, pixels_per_meter: float):
         self._origin_in_img = origin_in_img
@@ -101,7 +101,7 @@ class TrajectoryVisualizer:
     def draw_circle(self, img: np.ndarray, position: np.ndarray, **kwargs: Any) -> np.ndarray:
         """Draws the point as a circle on the image and returns it"""
         px_position = self._metric_to_pixel(position)
-        cv2.circle(img, tuple(px_position[::-1]), **kwargs)
+        img = cv2.circle(img, tuple(px_position[::-1]), **kwargs)
 
         return img
 

habitat-baselines/habitat_baselines/rl/ppo/utils/map/value_map.py
@@ -21,7 +21,7 @@ from .geometry_utils import (
     rotate_image,
 )
 
-DEBUG = False
+DEBUG = True
 SAVE_VISUALIZATIONS = False
 RECORDING = False
 PLAYING = False
@@ -42,6 +42,12 @@ class ValueMap(BaseMap):
     _min_confidence: float = 0.25
     _decision_threshold: float = 0.35
     _map: np.ndarray
+
+    # Frontiers markers
+    _circle_marker_radius: int = 5
+    _circle_marker_thickness: int = 2
+    _frontier_color: Tuple[int, int, int] = (0, 0, 255)
+    _selected__frontier_color: Tuple[int, int, int] = (0, 255, 255)
 
     def __init__(
         self,
@@ -194,7 +200,19 @@ class ValueMap(BaseMap):
         markers: Optional[List[Tuple[np.ndarray, Dict[str, Any]]]] = None,
         reduce_fn: Callable = lambda i: np.max(i, axis=-1),
         obstacle_map: Optional["ObstacleMap"] = None,  # type: ignore # noqa: F821
+        best_frontier: Optional[np.ndarray] = None,
     ) -> np.ndarray:
+
+        if markers is None:
+            markers = []
+            for frontier in obstacle_map._get_frontiers():
+                marker_kwargs = {
+                    "radius": self._circle_marker_radius,
+                    "thickness": self._circle_marker_thickness,
+                    "color": self._frontier_color,
+                }
+                markers.append((frontier[:2], marker_kwargs))
+
         """Return an image representation of the map"""
         # Must negate the y values to get the correct orientation
         reduced_map = reduce_fn(self._value_map).copy()
@@ -215,9 +233,12 @@ class ValueMap(BaseMap):
                 self._last_camera_yaw,
             )
 
-            if markers is not None:
-                for pos, marker_kwargs in markers:
-                    map_img = self._traj_vis.draw_circle(map_img, pos, **marker_kwargs)
+        if markers is not None:
+            for pos, marker_kwargs in markers:
+                if best_frontier is not None and np.array_equal(pos, best_frontier):
+                    marker_kwargs["color"] = self._selected__frontier_color
+                pos = self._px_to_xy(pos.reshape(1,2))[0]
+                map_img = self._traj_vis.draw_circle(map_img, pos, **marker_kwargs)
 
         return map_img
 
@@ -269,8 +290,8 @@ class ValueMap(BaseMap):
                 vis[point[1], point[0]] = (0, 255, 0)
             if SAVE_VISUALIZATIONS:
                 # Create visualizations directory if it doesn't exist
-                if not os.path.exists("visualizations"):
-                    os.makedirs("visualizations")
+                if not os.path.exists("images/debug"):
+                    os.makedirs("images/debug")
                 # Expand the depth_row back into a full image
                 depth_row_full = np.repeat(depth_row.reshape(1, -1), depth.shape[0], axis=0)
                 # Stack the depth images with the visible mask
@@ -281,10 +302,10 @@ class ValueMap(BaseMap):
                 vis_resized = cv2.resize(vis, (new_width, depth_rgb.shape[0]))
                 vis = np.hstack((depth_rgb, depth_row_full, vis_resized))
                 time_id = int(time.time() * 1000)
-                cv2.imwrite(f"visualizations/{time_id}.png", vis)
-            else:
-                cv2.imshow("obstacle mask", vis)
-                cv2.waitKey(0)
+                cv2.imwrite(f"images/debug/{time_id}.png", vis)
+            # else:
+            #     cv2.imshow("obstacle mask", vis)
+            #     cv2.waitKey(0)
 
         return visible_mask
 
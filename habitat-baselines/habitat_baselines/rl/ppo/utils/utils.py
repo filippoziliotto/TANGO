fix goat spl calculation + videos

habitat-baselines/habitat_baselines/rl/ppo/utils/utils.py
@@ -532,13 +532,19 @@ def log_final_results(
             aggregated_stats[stat_key] = np.mean(
                 [v[stat_key] for v in stats_episodes.values() if stat_key in v]
             )    
-        metrics = {k: v for k, v in aggregated_stats.items() if k != "reward"}
-        for k, v in metrics.items():
-            writer.add_scalar(f"eval_metrics/{k}", v, step_id)        
-                
-        writer.add_scalar(
-                "eval_reward/average_reward", aggregated_stats["reward"], step_id
+
+        # Remove infinite values and calculate mean
+        aggregated_stats['distance_to_goal'] = np.mean(
+            [v['distance_to_goal'] for v in stats_episodes.values() if v['distance_to_goal'] != float('inf')]
         )
+
+        metrics = {k: v for k, v in aggregated_stats.items() if k != "reward"}
+        # for k, v in metrics.items():
+        #     writer.add_scalar(f"eval_metrics/{k}", v, step_id)        
+                
+        # writer.add_scalar(
+        #         "eval_reward/average_reward", aggregated_stats["reward"], step_id
+        # )
             
         # Print final results
         logger.info('-----------------------')

habitat-lab/habitat/tasks/nav/nav.py
@@ -660,6 +660,8 @@ class SPL(Measure):
         self._sim = sim
         self._config = config
 
+        self.current_step = 0
+
         super().__init__()
 
     def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
@@ -672,12 +674,16 @@ class SPL(Measure):
 
         self._previous_position = self._sim.get_agent_state().position
         self._agent_episode_distance = 0.0
+
         self._start_end_episode_distance = task.measurements.measures[
             DistanceToGoal.cls_uuid
         ].get_metric()
+
         self.update_metric(  # type:ignore
             episode=episode, task=task, *args, **kwargs
         )
+
+        self.current_step = 0
 
     def _euclidean_distance(self, position_a, position_b):
         return np.linalg.norm(position_b - position_a, ord=2)
@@ -688,11 +694,25 @@ class SPL(Measure):
         ep_success = task.measurements.measures[Success.cls_uuid].get_metric()
 
         current_position = self._sim.get_agent_state().position
-        self._agent_episode_distance += self._euclidean_distance(
-            current_position, self._previous_position
-        )
+
+        # The distance is the current position to the goal
+        if (self.current_step == 0) and (episode.is_first_task == False):
+            self._agent_episode_distance = 0.
+            self._previous_position = current_position
+
+        else:
+            self._agent_episode_distance += self._euclidean_distance(
+                current_position, self._previous_position
+            )
 
         self._previous_position = current_position
+
+        # If subtask we take the distance to goal from current position
+        if (self.current_step == 1) and (episode.is_first_task == False):
+            self._start_end_episode_distance = task.measurements.measures[
+                DistanceToGoal.cls_uuid
+            ].get_metric()
+
 
         self._metric = ep_success * (
             self._start_end_episode_distance
@@ -700,6 +720,8 @@ class SPL(Measure):
                 self._start_end_episode_distance, self._agent_episode_distance
             )
         )
+
+        self.current_step += 1
 
 
 